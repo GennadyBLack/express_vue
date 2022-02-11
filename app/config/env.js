73e5408809f92b4769a377a6e4319e3964251e8b
El <richard.black183@gmail.com>
2022-02-11 08:46:39 +0300

    !fix conf

--- app/config/env.js
+++ app/config/env.js
@@ -1,17 +1,17 @@
 //settings for sequalize connection
 
 const env = {
-    database: 'postgres',
-    username: 'postgres',
-    password: 'test',
-    host: 'localhost',
-    dialect: 'postgres',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-  };
-   
-  module.exports = env;
+  database: "postgres",
+  username: "postgres",
+  password: "postgres", //test
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+module.exports = env;
